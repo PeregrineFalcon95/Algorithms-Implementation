d6bf18e7350f203aa37f86ddbce29a2118bca655
Amit Sarker <amitcse1995@gmail.com>
2022-09-21 06:32:44 +0600

    DSU with path compression

---
+++ Disjoint Set Union (DSU)/DSU with path compression.cpp
@@ -0,0 +1,61 @@
+/*
+Ref: https://cp-algorithms.com/data_structures/disjoint_set_union.html
+Category: DSU
+Sample Test Case:
+5 8
+2 1 2
+1 1 3
+1 2 1
+2 3 4
+1 1 3
+2 1 4
+1 1 4
+1 1 5
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+#define inf 1000000001
+#define fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie( NULL);
+int t, cases;
+
+
+class solution{
+private:
+    int findSet( int node, vector < int >& parent ){
+        if ( node == parent [ node ] )return node;
+        return parent [ node ] = findSet( parent [ node ], parent );
+    }
+    void unionSet(int firstNode, int secondNode, vector < int >& parent ){
+        firstNode = findSet(firstNode, parent);
+        secondNode = findSet(secondNode, parent);
+        if ( firstNode != secondNode )parent [ secondNode ] = firstNode;
+    }
+public:
+    void inputAndPerform(){
+        int n, m; //Num of nodes and operations
+        cin>>n>>m;
+        vector < int > parent (n+1);
+        //Make set
+        for ( int i = 1; i <= n; i++ )parent [ i ] = i;
+        while( m-- ){
+            int type, a, b;
+            cin>>type>>a>>b;
+            if ( type == 1 ){ // Find if same set
+                a = findSet(a,parent);
+                b = findSet(b,parent);
+                if ( a == b )cout<<"Same set\n";
+                else cout<<"Not same set\n";
+            }
+            else if ( type == 2 ){ //Join two set contains a and b
+                unionSet(a,b, parent);
+            }
+        }
+    }
+};
+int main(){
+    fast
+    solution obj;
+    obj.inputAndPerform();
+    return 0;
+}
